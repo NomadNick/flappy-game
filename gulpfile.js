3a0072a99de815dfcfd99ac5a504b043ee2ff034
NomadNick <emailnick123@gmail.com>
2015-06-09 18:10:29 -0700

    add dependencies

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+
+var jshint = require('gulp-jshint');
+var browserify = require('browserify');
+var uglify = require('gulp-uglify');
+var minifyHTML = require('gulp-minify-html');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+
